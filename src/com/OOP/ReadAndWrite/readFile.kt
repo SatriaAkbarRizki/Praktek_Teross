005da365a2eb51dbef0c82b38c58dc29fde699b0
linuxsar <sartiapl@gmai.com>
2022-11-01 22:49:31 +0700

    Add Program ReadAndWrite

---
+++ src/com/OOP/ReadAndWrite/readFile.kt
@@ -0,0 +1,42 @@
+package com.OOP.ReadAndWrite
+
+import java.util.*
+
+
+val  nameProgram = arrayOf(null,"Read File", "Write FIle", "Convert File to Word")
+
+val listProgram : (pilihan : Int) -> Unit = {
+    val showFile = readOperation()
+    when(it)
+    {
+        1 -> showFile.ReadShow()
+        else -> {
+            println("Pilihan not found")
+        }
+    }
+}
+
+
+
+fun main()
+{
+    val inputUser = Scanner(System.`in`)
+    print("Ingin Memakai Program Ini [Y/N] -> ")
+    val pilihanUser = inputUser.nextLine()
+//    val showFile = readOperation()
+    if(pilihanUser.equals("Y", ignoreCase = true))
+    {
+        for (x in 1..nameProgram.size){ println("[" + "$x" + "]" + " " + nameProgram[x])
+        if(x > 2){break}
+        }
+
+        print ("Pilih nomor program: "); val selectUser = inputUser.nextInt()
+        listProgram(selectUser)
+
+//        showFile.ReadShow()
+    }
+    else
+    {
+        println("Program Berhenti..")
+    }
+}
