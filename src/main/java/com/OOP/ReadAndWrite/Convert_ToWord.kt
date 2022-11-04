8ab062bbe9736413765fee3e31e1d10d82ab0c6f
linuxsar <sartiapl@gmai.com>
2022-11-04 17:54:30 +0700

    Update Program Menambahkan fitur convert to word

---
+++ src/main/java/com/OOP/ReadAndWrite/Convert_ToWord.kt
@@ -0,0 +1,65 @@
+package com.OOP.ReadAndWrite
+
+import com.aspose.words.Document
+import java.io.File
+import java.nio.file.Paths
+import java.util.*
+
+var nameFile : String = ""
+class Convert_ToWord {
+    val input = Scanner(System.`in`)
+    var collectionNameFile = ArrayList <String>()
+    val locationFile : String = Paths.get("").toAbsolutePath().toString()   // Lokasi File
+    var listDirectory : (whereLocation : String) -> Unit = {
+        File(it).list().forEach { collectionNameFile.add(it) } // Masukkin nama file ke array
+    }
+
+    fun ConvertNow()
+    {
+        listDirectory(locationFile)
+        (0..18).forEach{ print("=") }
+        println("\n\tList FIles")
+        (0..18).forEach{ print("=") }; println()
+        (collectionNameFile).forEach{ println(it) }
+        (0..18).forEach{ print("=") }; println()
+
+        getFile2(collectionNameFile)
+        println()
+        println("Convert Txt to Word, Now...")
+        println("Please Wait..")
+        nameFile.toWord(nameFile)
+    }
+}
+
+
+inline fun getFile2 (getCollectionName : ArrayList <String>) : Unit{
+    val input = Scanner(System.`in`)
+
+    print("Masukkan File yang ingin dibuka: "); val openFile = input.nextLine()
+    getCollectionName.forEach{ if (it.contains(openFile)){
+        println(it)
+        nameFile = it
+    }else if (it.contains(openFile) == null){
+            println("File tidak ditemukan");return@getFile2
+        }
+
+    }
+}
+
+infix fun String.toWord(name : String): Unit
+{
+    try
+    {
+        val doc = Document(name)
+        doc.save(name + ".docx")
+        println("Convert Succes")
+    }catch (e : Exception)
+    {
+        println(e)
+    }
+
+}
+
+
+
+
